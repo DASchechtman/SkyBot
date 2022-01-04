6d5a7fcfce6f97cba2bcd45fc452302776042cac
Dasiel96 <43630595+Dasiel96@users.noreply.github.com>
2022-01-04 03:28:25 -0600

    not much to clean up in the code because I was mainly debugging and modifying the build system to automatically set the bot in production mode without me manually setting it to such

--- switch_to_production.js
+++ switch_to_production.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
 const path = './.out/token.json'
-x = JSON.parse(fs.readFileSync(path))
-x["debug-mode"] = false
-fs.writeFileSync(path, JSON.stringify(x))
+const tokenInfo = JSON.parse(fs.readFileSync(path))
+tokenInfo["debug-mode"] = false
+fs.writeFileSync(path, JSON.stringify(tokenInfo))
